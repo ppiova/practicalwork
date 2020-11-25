feb2dfb91fba321af0bc9072d3b758d23346da97
Pablo <ppiova@gmail.com>
2020-11-25 19:35:44 -0300

    Clase 02 -  Xamarin

---
+++ Clase02/Clase02/Clase02/Clase02/Views/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Clase02.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ///UNO LLAMA A UN SERVICIO - API - CREDECIALES SON VALIDAS
+
+            if(entryUsuario.Text == "Mponce" && entryPass.Text =="goya12345")
+            {
+                DisplayAlert("Login", "Ingreso Exitoso", "OK");
+                //NAVEGATION X PAGE
+            }
+            else
+            {
+                //MENSAJE ERROR
+                DisplayAlert("Login", "Ingreso Fallido", "OK");
+            }
+        }
+    }
+}
---
+++ Clase02/Clase02/Clase02/Clase02/Views/Tiempo.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Clase02.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tiempo : ContentPage
+    {
+        public Tiempo()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int v, d, tiempo;
+
+            v = int.Parse(entryVelocidad.Text);
+            d = int.Parse(entryDistancia.Text);
+
+            tiempo = d / v;
+
+            lblRsultado.Text = tiempo.ToString();
+
+        }
+    }
+}
